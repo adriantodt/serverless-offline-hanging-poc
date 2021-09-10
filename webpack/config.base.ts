8c9b885cf8155793a39f8f163a4c808cf2d34849
Adrian Todt Monteiro da Silva <adriansilva@tokenlab.com.br>
2021-09-10 13:54:29 -0300

    Small changes

--- webpack/config.base.ts
+++ webpack/config.base.ts
@@ -1,40 +1,6 @@
 import { Configuration } from 'webpack';
 import { resolve } from 'path';
 
-const configuration = {
-  ignoredModules: [
-    // Unused - warnings
-    '@sap/hdbext',
-    'mongodb',
-    'mysql',
-    'mysql2',
-    'oracledb',
-    'pg-native',
-    'pg-query-stream',
-    'react-native-sqlite-storage',
-    'redis',
-    'ioredis',
-    'sql.js',
-    'sqlite3',
-    'mssql',
-    'typeorm-aurora-data-api-driver',
-    // Causes error
-    'pg-native',
-    'sharp',
-  ],
-  dependencyBundle: [
-    'dotenv',
-    'lodash',
-    'aws-sdk',
-    'typeorm',
-    'typedi',
-    'typeorm-typedi-extensions',
-    'pg'
-  ],
-}
-
-// BELOW: Implementation
-
 interface CustomWebpackParams {
   basePackage: string;
   entries: string[];
@@ -50,8 +16,6 @@ const basicWebpackConfig: Configuration = {
           transpileOnly: true,
           happyPackMode: true,
         },
-        test: /\.ts$/,
-        exclude: /node_modules/,
       },
     ],
   },
@@ -60,7 +24,6 @@ const basicWebpackConfig: Configuration = {
     alias: {
       '@': resolve('./src/'),
       '@test': resolve('../test/'),
-      'pg-native': resolve('./src/mock-pg-native.js'),
     },
   },
   output: {
@@ -75,21 +38,13 @@ const basicWebpackConfig: Configuration = {
   optimization: {
     minimize: false,
     nodeEnv: false,
-  },
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000,
-  },
-  externals: Object.fromEntries(configuration.ignoredModules.map((key) => [key, `commonjs ${key}`])),
+  }
 };
 
 export default function (params: CustomWebpackParams): Configuration {
   const entry: Record<string, any> = Object.fromEntries(params.entries.map(key => [key, {
     import: `${params.basePackage}/${key}/${params.entrypointName}`,
-    //dependOn: 'lib',
   }]));
-
-  //entry.lib = configuration.dependencyBundle;
 
   return { ...basicWebpackConfig, entry };
 }
